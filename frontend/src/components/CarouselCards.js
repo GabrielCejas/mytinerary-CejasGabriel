74a42e7142aad2c1d328b98f10f6c836d3d9d22d
Gabriel Cejas <gabrieldc20@hotmail.com>
2021-11-23 08:45:48 -0300

    Se trabajo en terminaciones

--- frontend/src/components/CarouselCards.js
+++ frontend/src/components/CarouselCards.js
@@ -24,7 +24,7 @@ function CarouselCards() {
   ];
 
   return (
-    <CardCarousel arrayCountries={arrayCountries} />
+    <CardCarousel arrayCountries={arrayCountries}  />
   );
 }
 export default CarouselCards;
