6643effe327d1d92972329abf2bf7c2f41f82eeb
Gabriel Cejas <gabrieldc20@hotmail.com>
2021-12-29 20:11:00 -0300

    heroku

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const passport = require("passport")
-require("./config/passport")
+const passport = require("passport");
+require("./config/passport");
 const app = express();
 app.use(express.json());
 
-require('./config/database')
+require("./config/database");
 
 const router = require("./routes/routes");
 
 app.use(cors());
-app.use(passport.initialize())
+app.use(passport.initialize());
 app.use("/api", router);
 
-app.listen(process.env.PORT || 4000, process.env.HOST || '0.0.0.0', () => {
-  console.log("El server esta escuchando");
+app.listen(process.env.PORT || 4000, process.env.HOST || "0.0.0.0", () => {
+  console.log(
+    `El server esta escuchando en el puerto ${process.env.PORT || 4000}`
+  );
 });
