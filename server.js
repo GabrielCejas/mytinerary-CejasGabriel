d145168c5ed9fe6ac37037f7d4139c82bd85aa3a
Gabriel Cejas <gabrieldc20@hotmail.com>
2021-12-29 19:13:37 -0300

    heroku

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use(cors());
 app.use(passport.initialize())
 app.use("/api", router);
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, process.env.HOST || '0.0.0.0', () => {
   console.log("El server esta escuchando");
 });
